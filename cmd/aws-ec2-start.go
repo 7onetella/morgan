9db896df4203fab562271fdd6be46e0ce47e0edc
7onetella <7onetella@gmail.com>
2019-05-30 02:34:14 -0400

    adding aws ec2 start

---
+++ cmd/aws-ec2-start.go
@@ -0,0 +1,65 @@
+// Copyright © 2019 Seven OneTella<7onetella@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/7onetella/morgan/tools/awsapi/ec2w"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+)
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:     "start <instance names>",
+	Short:   "Starts ec2",
+	Long:    `Starts ec2`,
+	Example: "start nginx redis",
+	Args:    cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		Newline()
+
+		instanceIDs, err := ec2w.GetInstanceIDsByNames(args)
+		ExitOn(err)
+		names := map[string]string{}
+
+		for i, name := range args {
+			names[instanceIDs[i]] = name
+		}
+
+		resp, err := ec2w.StartInstances(instanceIDs)
+		ExitOn(err)
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"Name", "Instance ID", "Prev", "Current"})
+
+		for _, c := range resp.StartingInstances {
+			table.Append([]string{names[*c.InstanceId], *c.InstanceId, string(c.PreviousState.Name), string(c.CurrentState.Name)})
+		}
+		table.Render()
+	},
+}
+
+func init() {
+	ec2Cmd.AddCommand(startCmd)
+}
